a5b11d1d74be4fabeec002cd2f4f3b0643ccd580
Andy Aguilar <aaguil3@gmail.com>
2022-12-04 20:38:52 -0500

    Update email template

--- index.ts
+++ index.ts
@@ -1,12 +1,19 @@
 import { AllDBEntriesResponse } from "./model/invitation-email/all-db-entries-response.interface";
 import { InvitationEmailDataObject } from "./model/invitation-email/invitation-email-data-object.interface";
 import { DBAllEntriesResponseToInvitationEmailConverter } from "./util/converter/db-entries-response-to-invitation-email.converter";
+import {
+  SESv2Client,
+  SendBulkEmailCommand,
+  BulkEmailEntry,
+} from "@aws-sdk/client-sesv2";
 
 var AWS = require("aws-sdk");
 
 AWS.config.update({ region: "us-east-2" });
 
 var ddb = new AWS.DynamoDB();
+
+const client = new SESv2Client({ region: "us-east-2" });
 
 const params = {
   ExpressionAttributeNames: {
@@ -18,7 +25,10 @@ const params = {
   TableName: "Rsvp-r3yc2rbtbnarxhyeptux54mu3e-prod",
 };
 
-const FILTER_EMAILS: string[] = ["aaguil3@gmail.com", "Kattyaaguilar@me.com"];
+const FILTER_EMAILS: string[] = [
+  "aaguil3@gmail.com",
+  "valerieaguilar430@gmail.com",
+];
 
 function isEmailValid(email: string | undefined): boolean {
   if (!email) {
@@ -28,16 +38,65 @@ function isEmailValid(email: string | undefined): boolean {
   return FILTER_EMAILS.includes(email);
 }
 
-function runScript(): void {
+async function runScript(): Promise<void> {
   ddb.scan(params, (err, data) => {
     if (err) {
-      console.log("Error: ", err);
+      console.error("Error: ", err);
     } else {
       const filteredRecords: (InvitationEmailDataObject | null)[] =
         getFilteredRecords(data.Items);
-      console.log(filteredRecords);
+
+      const sendBulkEmailCommandInput = {
+        BulkEmailEntries: getBulkEmailEntriesFromRecords(filteredRecords),
+        DefaultContent: {
+          Template: {
+            TemplateName: "rsvpEmail",
+            TemplateData: JSON.stringify({ addressLabel: "test", id: "1235" }),
+          },
+        },
+        FromEmailAddress: "TheFutureAguilars@kristinandandy.com",
+      };
+
+      console.log(sendBulkEmailCommandInput);
+
+      const command = new SendBulkEmailCommand(sendBulkEmailCommandInput);
+
+      console.log(command);
+
+      const response = client.send(command);
     }
   });
+}
+
+function getBulkEmailEntriesFromRecords(
+  records: (InvitationEmailDataObject | null)[]
+): BulkEmailEntry[] {
+  return records.map(getBulkEmailEntryFromRecord);
+}
+
+function getBulkEmailEntryFromRecord(
+  record: InvitationEmailDataObject | null
+): BulkEmailEntry {
+  if (!record) {
+    return {
+      Destination: {
+        ToAddresses: ["aaguil3@gmail.com"],
+      },
+    };
+  } else {
+    const { addressLabel, id } = record;
+
+    return {
+      Destination: {
+        ToAddresses: [record?.emailAddress],
+      },
+      ReplacementEmailContent: {
+        ReplacementTemplate: {
+          ReplacementTemplateData: JSON.stringify({ addressLabel, id }),
+        },
+      },
+    };
+  }
 }
 
 function getFilteredRecords(
